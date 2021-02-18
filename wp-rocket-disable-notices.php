a6b03db0f616eda0f6557e2936d171201d3e810d
Diego Giraldo <diego.dgiraldo36@gmail.com>
2021-02-18 12:59:38 -0500

    Create helper Plugin headers

---
+++ wp-rocket-disable-notices.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WP Rocket - Disable file permissions notices
+ * Description: Prevent WP Rocket from displaying warning notices about files not being writeable.
+ * Version: 0.0.1
+ * Requires PHP: 7.0
+ * Author: WP Media
+ * Licence: GPLv2 or later
+ *
+ * Copyright 2021 WP Rocket
+ * */
